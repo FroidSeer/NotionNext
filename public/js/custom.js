32e5d91035ef3ee40a5dbdca4cc72a287672de77
FroidSeer <61648238+FroidSeer@users.noreply.github.com>
2025-06-13 20:54:18 +0800

    Update custom.js
    添加了每日诗词的脚本

--- public/js/custom.js
+++ public/js/custom.js
@@ -1,1 +1,9 @@
 // 这里编写自定义js脚本；将被静态引入到页面中
+//每日诗词 自动替换id为jinrishici-sentence的span内容
+<script src="https://sdk.jinrishici.com/v2/browser/jinrishici.js" charset="utf-8"></script>
+<script type="text/javascript">
+  jinrishici.load(function(result) {
+    // 自己的处理逻辑
+    console.log(result)
+  });
+</script>
